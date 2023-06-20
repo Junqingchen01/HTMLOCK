dab7cede83ab50e7f71abea90fe9280f96b3f62a
Manuhps <92999699+Manuhps@users.noreply.github.com>
2023-06-20 15:21:11 +0100

    Portugues

--- js/rank.js
+++ js/rank.js
@@ -31,6 +31,6 @@ rankList.innerHTML = '';
 // 实现效果
 users.forEach((user, index) => {
   const li = document.createElement('li');
-  li.innerHTML = `Rank ${index + 1}: <span class="timewin"> ${user.nome}</span> TEMPO GANHA: <span class="timewin">${user.timewin}</span> S.`;
+  li.innerHTML = `Rank ${index + 1}: <span class="timewin"> ${user.nome}</span> PONTOS GANHOS: <span class="timewin">${user.timewin}</span> S.`;
   rankList.appendChild(li);
 });
